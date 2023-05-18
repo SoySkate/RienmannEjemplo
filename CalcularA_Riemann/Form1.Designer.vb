10a12db885d397c8b701a04c4b0e96b8e8715088
Formacio F3. 3 <formacio3@ibernyx.local>
2023-05-18 15:48:23 +0200

    ejemplocalculo

---
+++ CalcularA_Riemann/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxN = New System.Windows.Forms.TextBox()
+        Me.TextBoxResultado = New System.Windows.Forms.TextBox()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.BtnCalcular = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(242, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "N:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(202, 186)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Resultado:"
+        '
+        'TextBoxN
+        '
+        Me.TextBoxN.Location = New System.Drawing.Point(280, 109)
+        Me.TextBoxN.Name = "TextBoxN"
+        Me.TextBoxN.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxN.TabIndex = 2
+        '
+        'TextBoxResultado
+        '
+        Me.TextBoxResultado.Location = New System.Drawing.Point(280, 179)
+        Me.TextBoxResultado.Name = "TextBoxResultado"
+        Me.TextBoxResultado.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxResultado.TabIndex = 3
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Location = New System.Drawing.Point(671, 254)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSalir.TabIndex = 4
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = True
+        '
+        'BtnCalcular
+        '
+        Me.BtnCalcular.Location = New System.Drawing.Point(305, 244)
+        Me.BtnCalcular.Name = "BtnCalcular"
+        Me.BtnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCalcular.TabIndex = 5
+        Me.BtnCalcular.Text = "Calcular"
+        Me.BtnCalcular.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnCalcular)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.TextBoxResultado)
+        Me.Controls.Add(Me.TextBoxN)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxN As TextBox
+    Friend WithEvents TextBoxResultado As TextBox
+    Friend WithEvents BtnSalir As Button
+    Friend WithEvents BtnCalcular As Button
+End Class
---
+++ CalcularA_Riemann/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+
+    Function f(x As Decimal) As Decimal
+        Dim y As Decimal
+
+        y = CDec(Math.Sin(x))
+
+        'If x > 0 Then
+        '    y = Math.Sin(x)
+        'ElseIf x = 0 Then
+        '    MsgBox("x  = 0  Finalizado")
+        'ElseIf x < 0 Then
+        '    y = Math.Sin(x)
+        'End If
+
+        Return y
+    End Function
+
+    Function dameSignoyFuncion(x As Decimal) As Decimal
+        Dim r As Decimal
+        If Math.Sign(x) <> 0 Then
+            r = f(x)
+            Return r
+        Else
+            Return 0
+        End If
+    End Function
+
+    Private Function CArea() As Decimal
+        Dim i, n As Integer
+        Dim b, Base, h, pi, y, a, c, x, result As Decimal
+
+        pi = Math.PI
+
+        a = 1.5
+        c = (3 * pi) / 2
+
+
+        n = CInt(TextBoxN.Text)
+        'h = x / n
+
+
+
+        For i = 0 To n - 1
+            x = (a + c) / 2
+            y = dameSignoyFuncion(x)
+            ''El condicional esta mal, debo hacer que si x tiene el simbolo mayor que 0 significa
+            ''que el valor de a = x
+            'y al reves con el c (negativo)
+            If y > 0 Then
+                a = x
+            ElseIf y < 0 Then
+                c = x
+            ElseIf y = 0 Then
+                MsgBox("x = 0")
+            End If
+
+
+            'b = f(a + (i * h))
+            'Base = f(a + ((i + 1) * h))
+            'result += ((b + Base) / 2) * h
+        Next
+        TextBoxResultado.Text = y
+    End Function
+
+    'Function verificaciones(a As Decimal, c As Decimal, n As Integer) As String
+    '    If a = 0 Then
+    '        MsgBox("el valor a es igual a 0 por lo tanto ya sabemos qué es 0")
+    '    ElseIf c = 0 Then
+    '        MsgBox("el valor c es igual a 0 por lo tanto ya sabemos qué es 0")
+    '    ElseIf n = 0 Then
+    '        MsgBox("el valor n debe ser superior a 0")
+    '    ElseIf (a And c) < 0 Or (a And c) > 0 Then
+    '        MsgBox("los dos valores a y c no pueden tener el mismo signo")
+    '    End If
+
+    'End Function
+    Private Sub BtnCalcular_Click(sender As Object, e As EventArgs) Handles BtnCalcular.Click
+        'verificaciones(a, c, n)
+        CArea()
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        Me.Close()
+    End Sub
+End Class
